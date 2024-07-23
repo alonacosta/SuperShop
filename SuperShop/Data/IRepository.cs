9b1380f0871c199f84faaf48224c1b52e5cfe9bc
Alona <alona.costa2@gmail.com>
2024-07-23 12:12:27 +0100

    Repository

---
+++ SuperShop/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using SuperShop.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public interface IRepository
+    {
+        void AddProduct(Product product);
+        Product GetProduct(int id);
+        bool ProductExists(int id);
+        IEnumerable<Product> GetProducts();
+        void RemoveProduct(Product product);
+        Task<bool> SaveAllAsync();
+        void UpdateProduct(Product product);
+    }
+}
