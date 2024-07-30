1b2d55c105299b6c4c74d1716905803b2142b7bb
Alona <alona.costa2@gmail.com>
2024-07-30 07:37:05 +0100

    Add and edit images Images on API Products User on API Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -33,5 +33,17 @@ namespace SuperShop.Data.Entities
         public double Stock {  get; set; }
 
         public User User { get; set; }
+
+        public string ImageFullPath 
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+                return $"https://localhost:44331{ImageUrl.Substring(1)}";
+            }
+        }
     }
 }
